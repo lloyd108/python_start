daea5a2448c41ff6b6cc959848949b754fe6797f
lloyd <lloyd108@163.com>
2018-09-25 23:27:18 +0800

    2018/09/25: print graphs with for loop exercises 2

--- 00-BASIC/exercise-for1.py
+++ 00-BASIC/exercise-for1.py
@@ -17,12 +17,12 @@ print("* " * 5)
 print("* " * 5)
 print("* " * 5)
 
-# 利用for循环
-print("for loop1:")
+# use for-loop:
+print("for-loop1:")
 for i in range(0, 4):
     print("* " * 5)
 
-print("for loop2:")
+print("for-loop2:")
 for i in range(0, 4):
     for j in range(0, 5):
         print("*", end=" ")
@@ -34,7 +34,7 @@ for i in range(0, 4):
 *       *
 * * * * *
 """
-print("for loop3:")
+print("for-loop3:")
 for line in range(0, 4):
     for row in range(0, 5):
         if line in (0, 3) or row in (0, 4):
@@ -50,7 +50,7 @@ for line in range(0, 4):
 * * * *
 * * * * *
 """
-print("for loop4:")
+print("for-loop4:")
 for row in range(0,5):
     print("* " * (row + 1), end=" ")
     print("\n", end="")
@@ -63,12 +63,52 @@ for row in range(0,5):
 * * * * *
 """
 print("for loop5:")
-for row in range(5):
+side = 5
+for row in range(side):
     for col in range(row + 1):
-        if row == 2 and col == 1:
-            print("  ", end="")
-        elif row == 3 and col in (1, 2):
-            print("  ", end="")
-        else:
+        if row in (0, side - 1) and col in (0, row):
             print("* ", end="")
     print()
+
+"""
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+print("for-loop6:")
+for i in range(5, 0, -1):
+    print("* " * i, end="")
+    print()
+
+for i in range(5, 0, -1):
+    for j in range(i, 0, -1):
+        print("* ", end="")
+    print()
+
+"""
+* * * * *
+*     *
+*   *
+* *
+*
+"""
+print("for-loop7:")
+side = 5
+for i in range(side, 0, -1):
+    for j in range(i, 0, -1):
+        if i in (side, 1) or j in (i, 1):
+            print("* ", end="")
+        else:
+            print("  ", end="")
+    print()
+
+"""
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+"""
+print("for-loop8:")
