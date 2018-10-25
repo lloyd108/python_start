0b985582d438f2e5091bfdfed5ccdff3961db750
lloyd <lloyd108@163.com>
2018-10-25 23:39:36 +0800

    2018/10/25: advance: modules like time

--- 03-MODULES/myDatetime.py
+++ 03-MODULES/myDatetime.py
@@ -1,5 +1,31 @@
 import datetime
+from datetime import datetime as ddt
+import time
 
-print(type(datetime.date(2018, 10, 24)))
-print(datetime.date(2018, 10, 24))
+dt = datetime.date(2018, 10, 24)
 
+print(type(dt))
+print(dt)
+print(dt.day)
+print(dt.month)
+print(dt.year)
+
+print("*" * 20)
+
+my_dt = ddt(2018, 10, 24)
+print(my_dt.today())
+print(my_dt.now())
+print(ddt.today())
+print(ddt.now())
+print(ddt.fromtimestamp(time.time()))
+
+print("&" * 20)
+
+t1 = ddt.now()
+print(t1)
+print(t1.strftime("%Y%m%d %H%M%S"))
+
+td = datetime.timedelta(seconds=60)
+
+t2 = t1 + td
+print(t2.strftime("%Y%m%d %H%M%S"))
---
+++ 03-MODULES/myTimeit.py
@@ -0,0 +1,45 @@
+import timeit
+import time
+import sys
+import dis
+# help(timeit)
+
+sys.setrecursionlimit(99999)
+
+
+def factorial(a):
+    if a <= 1:
+        return 1
+    else:
+        return a * factorial(a - 1)
+
+
+t1 = time.time()
+factorial(2000)
+time.sleep(3)
+print(time.time() - t1)
+
+
+code1 = """
+sum = []
+for i in range(1000):
+    sum.append(i)
+"""
+
+code2 = """
+def factorial(a):
+    if a <= 1:
+        return 1
+    else:
+        return a * factorial(a - 1)
+
+factorial(2000)
+"""
+
+t1 = timeit.timeit(stmt="[i for i in range(1000)]", number=100000)
+t2 = timeit.timeit(stmt=code1, number=100000)
+t3 = timeit.timeit(stmt=code2, number=1000)
+t4 = timeit.timeit(stmt="time.sleep(2)", number=10)
+print(t1, t2, t3, t4, sep="/////")
+
+dis(factorial(3))
