838423caca474ee70e6266e8bc0a27a763be2a54
lloyd <lloyd108@163.com>
2019-01-05 15:22:57 +0800

    2019/1/5: js3

--- 13-Web/js/js4.js
+++ 13-Web/js/js4.js
@@ -5,9 +5,28 @@ window.onload = function() {
 	console.log(myDev1, typeof(myDev1));
 
 	// myDev1.style.color="red";
+	userInput = document.getElementById("uname")
+	console.log(userInput)
 };
 
 setRed = function(id){
 	var myDev2 = document.getElementById(id);
 	myDev2.style.color="red";
+	myDev2.style.width="500px";
+
+	console.log(myDev2.innerHTML)
+	myDev2.innerHTML="666666666666666666666"
+	myDev2.style.backgroundColor="yellow"
 };
+
+setBlack = function(id){
+	var myDev2 = document.getElementById(id);
+	myDev2.style.color="black";
+	myDev2.style.width="200px";
+
+	console.log(myDev2.innerHTML)
+	myDev2.innerHTML="black"
+
+	console.log(myDev2.style.width)
+};
+
